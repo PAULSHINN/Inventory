7c35bee49fc04d429e755b8ed9aa935e56f0f140
mrshinn <mr.shinn@yahoo.com>
2018-11-20 20:50:18 -0800

    fixed file that some dumbass named charles messed up

--- app/src/main/java/com/example/android/inventoryapp/CatalogActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/CatalogActivity.java
@@ -1,145 +1,119 @@
-package com.example.android.inventoryapp;
-
-import android.app.LoaderManager;
 import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.example.android.inventoryapp.data.BookContract;
+import com.example.android.inventoryapp.data.EditorActivity;
 
-// TODO 06: make CatalogActivity implement LoaderManager.LoaderCallbacks<Cursor>
-// this task will make cataogactivity red until you implement some methods
-public class CatalogActivity extends AppCompatActivity {
+import java.util.List;
 
-    // TODO 07: create a CONSTANT for url_loader and set it to 0
-
+public class CatalogActivity extends AppCompatActivity implements
+        LoaderManager.LoaderCallbacks<Cursor> {
+    /**
+     * Constant value for the URL LOADER.
+     **/
+    private static final int URL_LOADER = 0;
+    CursorAdapter mCursorAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
-
-        // TODO 01: make a variable for your list/recycle view, emptyview (if you use a listview) and assign it to the view
-        /**
-         * your activity_catalog.xml looks like its already set up from copying the pets layout. go find the id you need
-         */
-
-
-        // TODO 02  Make a new layout xml file list_item.xml
-        /**
-         * you can use the list_item from the pets app as a guide here. You need to have:
-         * the product name, the quantity, and a sale button in the layout. You can add any other info you want, but those are the things required.
-         * when you make the button view, add android:focusable="false" to the xml or you wont be able to click it later
-         */
-
-
-
-        // TODO 03: you need to make an adapter for your list/recyclerview.
-        /**
-         * this adapter needs to extend CursorAdapter because you will be passing in a cursor.
-         * use the pets adapter as a guide if you use a standard listview
-         */
-
-
-
-        // TODO 04: now that you have an adapter, made a new variable for it but set it to (this, null) so its empty
-
-
-        // TODO 05: set the adapter on the list/recycleview
-
-
-
-        // TODO 12: you need to set an onclick listener to the entire list item. this is different than the one you will make for the button
-        /**
-         * if you use the listview and not a recyclerview, you can use the AdapterView.onItemClickListener when you do .setOnItemClickListener. its in the pets app.
-         * this will launch an intent to the editor screen. you will send the id and the content uri in the intent
-         */
-
-
-
-
-        // TODO 08: use getLoaderManager.initLoader using the (url_loader variable, null, this) as arguments
-
-    }
-
-    // TODO 09: add override method for onCreateLoader
-    /**
-     * this will have a switch statement using the id
-     * if case is the url loader make a new CursorAdaptor and return that adapter
-     * if its not the url loader return null
-     */
-
-
-    // TODO 10: add override method for onLoaderFinished
-    /**
-     * this method will simple use .swapCursor on the adapter you made passing in the data variable
-     */
-
-
-
-    // TODO 11: add override method for onLoaderReset
-    /**
-     * this is the same as onLoaderFinished but you pass in null to swapCursor
-     */
-
-
-
-
-    private void insertBook() {
-        Log.i("MainActivity fart", "insertBook: clicked insertBook");
-        // Create a ContentValues object where column names are the keys,
-        // and Toto's pet attributes are the values.
-        ContentValues values = new ContentValues();
-        values.put(BookContract.BookEntry.COLUMN_PRODUCT_NAME, "I am the cheese.");
-        values.put(BookContract.BookEntry.COLUMN_PRICE, 11.99);
-        values.put(BookContract.BookEntry.COLUMN_QUANTITY, 45);
-        values.put(BookContract.BookEntry.COLUMN_SUPPLIER_NAME, "Barnes and Noble");
-        values.put(BookContract.BookEntry.COLUMN_SUPPLIER_PHONE, "(555) 555-5555");
-
+        private void insertBook () {
+            Log.i("MainActivity fart", "insertBook: clicked insertBook");
+            // Create a ContentValues object where column names are the keys,
+            // and Toto's pet attributes are the values.
+            ContentValues values = new ContentValues();
+            values.put(BookContract.BookEntry.COLUMN_PRODUCT_NAME, "I am the cheese.");
+            values.put(BookContract.BookEntry.COLUMN_PRICE, 11.99);
+            values.put(BookContract.BookEntry.COLUMN_QUANTITY, 45);
+            values.put(BookContract.BookEntry.COLUMN_SUPPLIER_NAME, "Barnes and Noble");
+            values.put(BookContract.BookEntry.COLUMN_SUPPLIER_PHONE, "(555) 555-5555");
+            // Find the ListView which will be populated with the pet data
+            ListView list = findViewById(R.id.list);
+            // Find and set empty view on the ListView, so that it only shows when the list has 0 items.
+            View emptyView = findViewById(R.id.empty_view);
+            list.setEmptyView(emptyView);
+            // Setup an Adapter to create a list item for each row of pet data in the Cursor.
+            // There is no pet data yet (until the loader finishes) so pass in null for the Cursor.
+            mCursorAdapter = new CursorAdapter(this, null);
+            list.setAdapter(mCursorAdapter);
+            // Setup the item click listener
+            list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                    // Create new intent to go to {@link EditorActivity}
+                    Intent intent = new Intent(CatalogActivity.this, EditorActivity.class);
+                    // Form the content URI that represents the specific pet that was clicked on,
+                    // by appending the "id" (passed as input to this method) onto the
+                    // {@link PetEntry#CONTENT_URI}.
+                    // For example, the URI would be "content://com.example.android.pets/pets/2"
+                    // if the pet with ID 2 was clicked on.
+                    Uri currentBookUri = ContentUris.withAppendedId(BookEntry.CONTENT_URI, id);
+                    // Set the URI on the data field of the intent
+                    intent.setData(currentBookUri);
+                    // Launch the {@link EditorActivity} to display the data for the current pet.
+                    startActivity(intent);
+                }
+            });
+            // Kick off the loader
+            getLoaderManager().initLoader(URL_LOADER, null, this);
+        }
         Uri newUri = getContentResolver().insert(BookContract.BookEntry.CONTENT_URI, values);
         Log.i("MainActivity fart", "insertBook: newUri is: " + newUri);
     }
-
     /**
-     * Helper method to delete all pets in the database.
+     * Helper method to delete all books in the database.
      */
     private void deleteAllBooks() {
         int rowsDeleted = getContentResolver().delete(BookContract.BookEntry.CONTENT_URI, null, null);
-        Log.v("CatalogActivity", rowsDeleted + " rows deleted from pet database");
+        Log.v("CatalogActivity", rowsDeleted + " rows deleted from book database");
     }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-
             case R.id.action_insert_dummy_data:
                 Log.i("Options fart", "onOptionsItemSelected: dummy data clicked");
                 insertBook();
-
             case R.id.action_delete_all_entries:
                 deleteAllBooks();
         }
-
         return super.onOptionsItemSelected(item);
     }
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_catalog, menu);
-        return true;
+    public Loader<List<Books>> onCreateLoader(int i, Bundle bundle) {
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        StringBuilder stringBuilder = new StringBuilder();
+        return new BooksLoader(this, stringBuilder.toString());
+    }
+    @Override
+    public void onLoadFinished(Loader<List<Books>> loader, List<Books> booksList) {
+        // Hide loading indicator because the data has been loaded
+        View loadingIndicator = findViewById(R.id.loading_indicator);
+        loadingIndicator.setVisibility(View.GONE);
+        // Set empty state text to display "No Articles Found"
+        mEmptyStateTextView.setText(R.string.no_books);
+        // Clear the adapter of previous article data
+        mAdapter.clear();
+        // If there is a valid list of {@link Books} articles, then add them to the adapter's data set
+        // This will trigger the ListView to update
+        if (books != null && !books.isEmpty()) {
+            mAdapter.addAll(books);
+        }
+    }
+    @Override
+    public void onLoaderReset(Loader<List<BookContract.BookEntry>> loader) {
+        // Loader reset, so we can clear out our existing data.
+        mAdapter.clear();
     }
 }
-
-// TODO 13: make a new activity called EditorActivity
-/**
- * once you are this far we will revisit your current progress
- */
